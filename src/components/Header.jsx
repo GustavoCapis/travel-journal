025c59085281367a321bb78b85d06dd1e56d7799
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-02-09 15:34:06 -0300

    fix: Update  assets path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,9 @@
-/**
- * Challenge:
- *
- * - Create an App component in a separate file.
- *   Import it here and render it
- * - Create a "components" folder and a Header component.
- *   Render the Header inside the App component.
- * - Follow the Travel Journal design to build the Header
- *   for our page.
- */
+import globeIcon from "../imgs/globe.png";
 
 export default function Header() {
   return (
     <header className="header">
-      <img className="header-icon" src="/src/imgs/globe.png" alt="globe-icon" />{" "}
+      <img className="header-icon" src={globeIcon} alt="globe-icon" />
       <small className="header-title">my travel journal.</small>
     </header>
   );
